632e1172882539b32803f003263db26a12d19247
Reinnovs <thaboroy@gmail.com>
2020-05-03 05:19:16 +0200

    Refactoring with Eclipse

--- src/com/reinnovs/firstjavaproject/MultiplicationTable.java
+++ src/com/reinnovs/firstjavaproject/MultiplicationTable.java
@@ -4,17 +4,13 @@ public class MultiplicationTable {
 	// 5*1 = 5
 	// 5*10 = 50
 	void print() {
-		for (int x = 1; x <= 10; x++) {
-			System.out.printf("%d * %d = %d", 5, x, 5 * x).println();
-		}
+		print(5);
 	}
 
 	void print(int table) {
-		for (int y = 1; y <= 10; y++) {
-			System.out.printf("%d * %d = %d", table, y, table * y).println();
-		}
+		print(table, 1, 10);
 	}
-	
+
 	void print(int table, int from, int to) {
 		for (int y = from; y <= to; y++) {
 			System.out.printf("%d * %d = %d", table, y, table * y).println();
--- src/com/reinnovs/firstjavaproject/MultiplicationTableRunner.java
+++ src/com/reinnovs/firstjavaproject/MultiplicationTableRunner.java
@@ -4,9 +4,9 @@ public class MultiplicationTableRunner {
 
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
-//		table.print();
+		table.print();
 //		table.print(5);
-		table.print(5, 2, 12);
+//		table.print(5, 2, 12);
 	}
 
 }
