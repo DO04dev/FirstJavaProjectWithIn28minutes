754859214677a2176616f13aac7518b0bdc670cd
Reinnovs <thaboroy@gmail.com>
2020-05-03 05:08:25 +0200

    Adding More Methods for multiplication Table Program

--- src/com/reinnovs/firstjavaproject/MultiplicationTable.java
+++ src/com/reinnovs/firstjavaproject/MultiplicationTable.java
@@ -1,11 +1,23 @@
 package com.reinnovs.firstjavaproject;
 
 public class MultiplicationTable {
-	//5*1 = 5
-	//5*10 = 50
+	// 5*1 = 5
+	// 5*10 = 50
 	void print() {
-		for(int x=1;x<=10;x++) {
-			System.out.printf("%d * %d = %d", 5, x, 5*x).println();
+		for (int x = 1; x <= 10; x++) {
+			System.out.printf("%d * %d = %d", 5, x, 5 * x).println();
+		}
+	}
+
+	void print(int table) {
+		for (int y = 1; y <= 10; y++) {
+			System.out.printf("%d * %d = %d", table, y, table * y).println();
+		}
+	}
+	
+	void print(int table, int from, int to) {
+		for (int y = from; y <= to; y++) {
+			System.out.printf("%d * %d = %d", table, y, table * y).println();
 		}
 	}
 }
