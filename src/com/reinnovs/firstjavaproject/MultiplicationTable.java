c17cd00f0637fefcf5b95b93bf1ef1073715f9bf
Reinnovs <thaboroy@gmail.com>
2020-05-03 04:45:59 +0200

    Writing Multiplication Table Java Program with Eclipse

---
+++ src/com/reinnovs/firstjavaproject/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.reinnovs.firstjavaproject;
+
+public class MultiplicationTable {
+	//5*1 = 5
+	//5*10 = 50
+	void print() {
+		for(int x=1;x<=10;x++) {
+			System.out.printf("%d * %d = %d", 5, x, 5*x).println();
+		}
+	}
+}
---
+++ src/com/reinnovs/firstjavaproject/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package com.reinnovs.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+
+	}
+
+}
